4ff2ff3305b409e71ce79a1f8ad0ea0568c66fca
root <root@ip-172-31-9-41.us-east-2.compute.internal>
2021-05-21 01:16:48 +0000

    delete debugging leftover

--- edge/pkg/metamanager/process.go
+++ edge/pkg/metamanager/process.go
@@ -461,7 +461,6 @@ func (m *metaManager) processDelete(message model.Message) {
 }
 
 func (m *metaManager) processQuery(message model.Message) {
-	//dao.DeleteMetaByType("mission")
 	resKey, resType, resID := parseResource(message.GetResource())
 	var metas *[]string
 	var err error
